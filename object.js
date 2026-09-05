068934d6e6f237f1711db61ad70bc4865822cf07
MD. ABIR <mail.er.abir@gmail.com>
2026-09-06 02:42:54 +0600

    we can use more than one variable in one object and access them by a . operator and also convert them into array element by using entries,keys,vlues keyword

--- object.js
+++ object.js
@@ -1,15 +1,18 @@
-let user ={
+let user = {
   name : "Abir",
   age : 21,
   address : "Dhaka",
 };
-//console.log(user.name);
-//console.log(user["name"]);
+// console.log(user);
+// console.log(user.name);
+// console.log(user["name"]);
 delete user.address;
-console.log(user);
 user.address = "Manikganj";
 user.address = {
-    city : "jhitka",
-    area : "harirampur",
+  city : "Manikganj",
+  area : "Uposohor",
 };
-console.log(user);
+// console.log(user);
+console.log(Object.entries(user));
+console.log(Object.keys(user));
+console.log(Object.values(user));
