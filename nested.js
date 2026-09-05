2d2e0b20219a5882a951bbbee28b1697e5d6c703
MD. ABIR <mail.er.abir@gmail.com>
2026-09-06 03:37:52 +0600

    Explained the nesting of array and the array of objects

--- nested.js
+++ nested.js
@@ -10,5 +10,31 @@ let user = {
   },
 };
 
-console.log(user["address"]["zipcode"]);
+//console.log(user["address"]["zipcode"]);
 // console.log(user.address.zipcode);
+// let entry = Object.entries(user);
+// console.log(entry[0][1]);
+let students = [
+    {
+        name: "Abir",
+        id : 302,
+        address :{
+            area : "Savar",
+            thana : "Dhamrai",
+            lane : "Madrasa road",
+            option: ["Nilachol","selfie","D-link"],
+        },
+    },
+    {
+        name: "Sabir",
+        id : 302,
+    },
+    {
+        name: "Kabir",
+        id : 302,
+    },
+];
+
+
+students[0].address.option[1] = "SBsuper" ;
+console.log(students[0].address.option[1]);
