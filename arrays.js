7147ef9aa458ce2304bd20d51e237960cf3c1fb4
MD. ABIR <mail.er.abir@gmail.com>
2026-09-06 02:02:45 +0600

    for adding an element in 0 index we used unshift() and removing that element from that of 0 index we use shift() methods

--- arrays.js
+++ arrays.js
@@ -1,8 +1,10 @@
 let name = ["abir","rohim","korim"];
 let number = [2,4,5];
 // console.log(name[1],number[1]);
-name .push("zodu");
-name.push("abul");
-name.push("kabul");
-name.pop();
+// name .push("zodu");
+// name.push("abul");
+// name.push("kabul");
+// name.pop();
+name.unshift("kabul");
+name.shift();
 console.log(name);
