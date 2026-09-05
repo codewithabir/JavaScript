dfcfb04b36c43a2d494993c8bab780f5a7add3e8
MD. ABIR <mail.er.abir@gmail.com>
2026-09-06 03:03:12 +0600

    nested object in javascript

--- nested.js
+++ nested.js
@@ -1,16 +1,14 @@
 const { use } = require("react");
 
-let user ={
-    name : "abir",
-    age : 22,
-    address: {
-        city: "Manikganj",
-        area : "Upashahar",
-        zipcode: 1800,
-    },
-
+let user = {
+  name: "Abir",
+  age: 21,
+  address: {
+    city: "Manikganj",
+    area: "Uposohor",
+    zipcode: 1800,
+  },
 };
-//console.log(user["address"]["zipcode"]);
-//console.log(user.address.area);
-let entry = Object.entries(user);
-console.log(entry[0][1]);
+
+console.log(user["address"]["zipcode"]);
+// console.log(user.address.zipcode);
