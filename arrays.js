3889fd18073a35b5b0b86b5c045c4c777f02884b
MD. ABIR <mail.er.abir@gmail.com>
2026-09-06 01:58:01 +0600

    adding element into arary using push() and removing element from the array using pop()

--- arrays.js
+++ arrays.js
@@ -1,3 +1,8 @@
 let name = ["abir","rohim","korim"];
 let number = [2,4,5];
-console.log(name[1],number[1]);
+// console.log(name[1],number[1]);
+name .push("zodu");
+name.push("abul");
+name.push("kabul");
+name.pop();
+console.log(name);
