f9a08ab61cb12c8fbe7183e62126fd4d3d43aee6
MD. ABIR <mail.er.abir@gmail.com>
2026-09-20 00:02:14 +0600

    Loops in javaScript

---
+++ loop.js
@@ -0,0 +1,14 @@
+// for( let i = 0; i<5 ; i++){
+//     console.log( `count: ${i}` );
+// }
+
+let fruits = [ "Mango", "Jackfruit","Apple","Orange" ];
+// for( let i = 0 ; i< fruits.length ; i++){
+//     console.log("Khabo: ",fruits[i]);
+// }
+
+let i = 0;
+while(i< fruits.length){
+    console.log("Khabo: ", fruits[i]);
+    i++;
+}
